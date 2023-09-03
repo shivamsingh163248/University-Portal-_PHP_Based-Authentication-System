861f4bf48c7ba5019ab248b61d7a10b78a02764e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-04 01:25:00 +0530

    changed

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /*this file contain detabase configation asumaing you are running mysql usering usesr root and apsward */
 define('DB_SERVER' , 'localhost');
-define('DB_USERNAME' , 'DATABASE_USER_NAME');
-define('DB_PASSWORD' , 'FTP_DATA_PSW');
-define('DB_NAME' , 'DATABSENAME');
+define('DB_USERNAME' , 'u348475756_shivam');
+define('DB_PASSWORD' , '@Itisha8');
+define('DB_NAME' , 'u348475756_userdetail');
 
 
 $conn = mysqli_connect (DB_SERVER, DB_USERNAME , DB_PASSWORD , DB_NAME);
