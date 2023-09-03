29a770cacaff6c4e8e469d59fb7d1e81e58097aa
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-04 01:19:14 +0530

    changed

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 /*this file contain detabase configation asumaing you are running mysql usering usesr root and apsward */
 define('DB_SERVER' , 'localhost');
-define('DB_USERNAME' , 'root');
-define('DB_PASSWORD' , '');
-define('DB_NAME' , 'login');
+define('DB_USERNAME' , 'DATABASE_USER_NAME');
+define('DB_PASSWORD' , 'FTP_DATA_PSW');
+define('DB_NAME' , 'DATABSENAME');
 
 
 $conn = mysqli_connect (DB_SERVER, DB_USERNAME , DB_PASSWORD , DB_NAME);
