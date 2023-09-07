7250e565e06640193c3be516f988f585e3474b68
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-07 05:50:34 +0530

    update

--- welcome.php
+++ welcome.php
@@ -9,105 +9,131 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title> Ionic SmartWatch - LandingPage</title>
-    <link rel="stylesheet" href="welcome.css">
-    <script src="https://kit.fontawesome.com/a2291e6430.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    
+  <title> Ionic SmartWatch - LandingPage</title>
+  <link rel="stylesheet" href="wel.css">
+  <link rel="stylesheet" href="welcomesec2.css">
+  <script src="https://kit.fontawesome.com/a2291e6430.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&display=swap" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise" rel="stylesheet" type="text/css"/>
+  <link rel="stylesheet" href="style.css" /> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+//Body Section
+<script src="usincplyaer.js"></script>
+<link rel="stylesheet" href="musicplayer.css">
 
 </head>
 
 <body>
-    <div class="container">
-        <header class="nav" id="and_menor">
-            <div class="nav-container-content">
-                <nav class="navbar">
-                    <a href="#">Home</a>
-                    <a href="#">About</a>
-                    <a href="#">Products</a>
-                    <a href="#">Buy Now</a>
-                </nav>
-            </div>
-        </header>
 
-        <div class="section-container-items">
-            <div class="section-items-col-12">
-                <div class="section">
-                    <h1>Smartwatch fitbit ionic.</h1>
-                    <h3>Altimeter, Gyroscope, GPS, Heart Rate Monitor</h3>
-                    <a href="#"><button><i class="fas fa-shopping-cart"></i>Buy now for $244.60</button></a>
-                </div>
 
-                <div class="alta">
-                    <span class="alta-title">Find Yours</span>
-                    <img
-                        src="https://static-geektopia.com/storage/geek/products/fitbit/versa/versa-black-black-aluminum-0-4d70f54ede557d65061a77d521339857.png">
-                </div>
-            </div>
+ <section class="header">
+
+  <div class="container">
+    <header class="nav" id="and_menor">
+      <div class="nav-container-content">
+        <nav class="navbar">
+          <a href="welcome.php">Home</a>
+          <a href=" ">Massage</a>
+          <a href="logout.php">Log Out</a>
+        </nav>
+      </div>
+    </header>
+
+
+    <div class="section-container-items">
+      <div class="section-items-col-12">
+        <div class="section">
+
+  <h1>Hi !</h1>
+
+          <h1>
+            <a href="" class="typewrite" data-period="2000" data-type='[ "itisha ", "now i will connect to you", "always something update for you ", "check website reguraly" ]'>
+              <span class="wrap"></span>
+            </a>
+          </h1>
+          <h3>congrualtion if you are present hear </h3>
+
+
+
+
+
+          <a href="#"><button><i class="fa fa-chevron-down">  Down  </i></button></a>
+       
         </div>
+
+
+
+      </div>
     </div>
+  </div>
+ </section>
+
+
+<section class="welcomesection2">
+
+
+<h1 class="massage">Massage </h1>
+
+  <div  id="typedtext" > </div>
+
+
+
+
+</section>
+
+
+
+<section class="thirdsecton">
+
+<h1 class="massageE">SONG FOR YOU </h1>
+
+
+
+<h1>Music Player</h1>
+<div class="music-container" id="music-container">
+<div class="music-info">
+<h4 class="title" id="title"></h4>
+<div class="progress-container" id="progress-container">
+<div class="progress" id="progress"></div>
+</div>
+</div>
+<audio src="./music/heeriye.mp3" id="audio"></audio>
+<div class="img-container">
+<img src="./images/unnamed.png" alt="music-cover" id="cover" />
+</div>
+<div class="navigation">
+<button id="prev" class="action-btn">
+<i class="fa fa-backward" aria-hidden="true"></i>
+</button>
+<button id="play" class="action-btn action-btn-big">
+<i class="fa fa-play" aria-hidden="true"></i>
+</button>
+<button id="next" class="action-btn">
+<i class="fa fa-forward" aria-hidden="true"></i>
+</button>
+</div>
+</div>
+
+
+
+
+</section>
+
+
+
+
+
+
 </body>
+<script src="welce.js"></script>
 
-</html>
-
-
-
-
-
-
-
-<?php 
-
-// Include configuration file  
-require ('config.php');
-
-$user_name_this =  $_SESSION['username']; 
-
-
- 
-//Select query 
-$select = "SELECT * FROM vbspu WHERE username  = '$user_name_this' " ;
-$result = $conn->query($select);
-?> 
-
-<p>
-
-
-<?php   
-			// Loop the employee data
-				echo "this is the print name ";
-					
-
-                    if ($row = $result->fetch_object())
-                   
-                    
-                    {
-                        echo $row->yourname ;
-      
-                    }
-?>
-
-</p>
-
-
-
-
-
-</p>
-
-
-</body>
-<script src="welcome.js"></script>
 </html>
