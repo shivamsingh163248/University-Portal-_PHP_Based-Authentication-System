3bf9e688436e2b4bde53194ad3fa39f2f22c7027
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2023-09-07 05:56:57 +0530

    changed

--- welcome.php
+++ welcome.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
   <link href="https://fonts.googleapis.com/css?family=Waiting+for+the+Sunrise" rel="stylesheet" type="text/css"/>
   <link rel="stylesheet" href="style.css" /> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-//Body Section
+
 <script src="usincplyaer.js"></script>
 <link rel="stylesheet" href="musicplayer.css">
 
